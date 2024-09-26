1a8375e12afbd782b8b5b4d504ad6e139192378a
rdomon <dom21008@byui.edu>
2024-09-26 17:39:59 -0600

    mission 2

--- w01/mission/mission.js
+++ w01/mission/mission.js
@@ -1,8 +1,14 @@
-const themeSelector = // replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
+const themeSelector = document.querySelector('#theme-select');// replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
 function changeTheme() {
 // check to see what the current value of our select is.
 // The current value is conveniently found in themeSelector.value!
-
+    if (themeSelector.value === 'dark'){
+        document.body.classList.add('dark');
+        document.querySelector('.logo').src = 'byui-logo_white.webp';
+    } else{
+        document.body.classList.remove('dark');
+        document.querySelector('.logo').src = 'byui-logo_white.png';
+    }
 // if the value is dark then:
 // add the dark class to the body
 // change the source of the logo img to point to the white logo.
