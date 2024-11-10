9c05296b9cff41c8b9f2e368a29dbb63b46f3772
rdomon <dom21008@byui.edu>
2024-11-09 22:40:08 -0700

    registration

---
+++ w08/ofject.js
@@ -0,0 +1,18 @@
+const obj = {
+    key: 'value',
+    'key1': 'value'
+}
+console.log(obj.key)
+console.log(obj['key'])
+
+// const document = {
+//     getElementById: (theIDOfTheElement){
+//         return theIDOfTheElement
+//     }
+// }
+
+document.getElementById('Id')
+
+
+// const memory = 0x10098234
+// const memory = 0x10098234
